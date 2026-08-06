9d489391d8921ac1fdbca0fb03c660ac901f1863
Bryant Nguyen <96253282+sleanguyen@users.noreply.github.com>
2026-08-06 11:49:18 +0700

    fix: new fix in fix

--- server/api/routes/authRoutes.js
+++ server/api/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controller/authController');
+const authController = require('../controllers/authController');
 const { verifyToken } = require('../../middleware/authMiddleware');
 
 router.post('/register', authController.register);
